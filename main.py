513db726d9575ea388216d071232b7f1da48ab0c
John Ric Merque <johnricmer@gmail.com>
2023-02-06 22:36:33 +0800

    added a function to get the bare response

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import random
 import long_responses as long
 
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower()) # removes punctuations
+    response = check_all_messages(split_message)
+    return response
+
 # Testing the response system
 while True:
     print('Bot: ' + get_response(input('You: ')))
