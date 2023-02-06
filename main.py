53028d1bfadbd13475a6d7a4181558637c931442
John Ric Merque <johnricmer@gmail.com>
2023-02-06 22:51:45 +0800

    checking required words in user message

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def message_probability(user_message, recognised_words, single_response=False, r
     # Calculates the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
 
+    # Checks that the required words are in the string
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+            
 def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower()) # removes punctuations to get bare words
     response = check_all_messages(split_message)
