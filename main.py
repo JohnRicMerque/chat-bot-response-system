ba9aeacbc27c46966bdd6e59c4fbf7d017959436
John Ric Merque <johnricmer@gmail.com>
2023-02-06 22:41:39 +0800

    initialized a function that calculates message probability

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import random
+import re
 import long_responses as long
+
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    # Counts how many words are present in each predefined message
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
 
 def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower()) # removes punctuations
