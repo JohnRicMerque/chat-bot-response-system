59b807a998fc729363935ae01c65d82c4aef666a
John Ric Merque <johnricmer@gmail.com>
2023-02-06 22:44:32 +0800

    added an accuracy percentage for recognized words

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def message_probability(user_message, recognised_words, single_response=False, r
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
+    
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
 
 def get_response(user_input):
-    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower()) # removes punctuations
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower()) # removes punctuations to get bare words
     response = check_all_messages(split_message)
     return response
 
